b5c1e0b493ad15982b08f7011985d2d2e3aec6ff
ahmednabeen <ahmednabeen@users.noreply.github.com>
2026-06-14 21:35:23 +0600

    Add bulk actions to OrderAdmin for status changes

--- main/admin.py
+++ main/admin.py
@@ -33,10 +33,29 @@ from .models import Order as OrderModel
 
 @admin.register(OrderModel)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'address', 'status', 'total', 'created_at')
+    list_display = ('id', 'name', 'address', 'phone', 'status', 'total', 'created_at')
     list_filter = ('status', 'created_at')
     search_fields = ('name', 'address', 'phone', 'email')
     list_editable = ('status',)
+    list_per_page = 20
+    ordering = ('-created_at',)
+
+    actions = ['mark_preparing', 'mark_out_for_delivery', 'mark_delivered']
+
+    def mark_preparing(self, request, queryset):
+        updated = queryset.update(status='Preparing')
+        self.message_user(request, f'{updated} order(s) marked as Preparing.')
+    mark_preparing.short_description = 'Mark selected orders as Preparing'
+
+    def mark_out_for_delivery(self, request, queryset):
+        updated = queryset.update(status='Out for Delivery')
+        self.message_user(request, f'{updated} order(s) marked as Out for Delivery.')
+    mark_out_for_delivery.short_description = 'Mark selected orders as Out for Delivery'
+
+    def mark_delivered(self, request, queryset):
+        updated = queryset.update(status='Delivered')
+        self.message_user(request, f'{updated} order(s) marked as Delivered.')
+    mark_delivered.short_description = 'Mark selected orders as Delivered'
 
 
 @admin.register(BlogPost)
